7f58a5e3639c24a74843e7849fc8a3133140e79a
Yannick Dominguez <yannick.dominguez@gmail.com>
2015-03-19 14:53:10 -0700

    updated build repo

--- nightly.sh
+++ nightly.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git clone git@github.com:yanhick/intellij-haxe.git
+git clone git@github.com:tivo/intellij-haxe.git
 cd intellij-haxe
-git remote add nightly git@github.com:yanhick/intellij-haxe.git
+git remote add nightly git@github.com:yanhick/intellij-haxe-nightly-builds.git
 DATE=`date +"%m-%d-%Y--%H-%M"`
 git tag $DATE
 git push nightly --tags

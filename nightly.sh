f9c62fae10b5a405c3c9ae53ae8c18c47e0cc68b
Yannick Dominguez <yannick.dominguez@gmail.com>
2015-03-19 14:33:11 -0700

    add hour and minutes to timestamp

--- nightly.sh
+++ nightly.sh
@@ -3,7 +3,7 @@
 git clone git@github.com:yanhick/intellij-haxe.git
 cd intellij-haxe
 git remote add nightly git@github.com:yanhick/intellij-haxe.git
-DATE=`date +"%m-%d-%Y"`
+DATE=`date +"%m-%d-%Y--%H-%M"`
 git tag $DATE
 git push nightly --tags
 cd ../

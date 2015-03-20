c57757e4d2a637c0ebf5e0b3d552a888789ef9c8
Yannick Dominguez <yannick.dominguez@gmail.com>
2015-03-20 12:21:21 -0700

    merge nightly repo code before pushing to it

--- nightly.sh
+++ nightly.sh
@@ -3,6 +3,8 @@
 git clone git@github.com:tivo/intellij-haxe.git
 cd intellij-haxe
 git remote add nightly git@github.com:yanhick/intellij-haxe-nightly-builds.git
+git fetch nightly
+git merge nightly/master
 DATE=`date +"%m-%d-%Y--%H-%M"`
 git tag $DATE
 git push nightly master

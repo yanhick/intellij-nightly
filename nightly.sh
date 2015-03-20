ec0badcd6603fe8e2c8d1c89e0cf40410eee2bd1
Yannick Dominguez <yannick.dominguez@gmail.com>
2015-03-20 12:03:48 -0700

    also push code to master of nightly repo

--- nightly.sh
+++ nightly.sh
@@ -5,6 +5,7 @@ cd intellij-haxe
 git remote add nightly git@github.com:yanhick/intellij-haxe-nightly-builds.git
 DATE=`date +"%m-%d-%Y--%H-%M"`
 git tag $DATE
+git push nightly master
 git push nightly --tags
 cd ../
 rm -rf intellij-haxe

33de46d2f089433f5838203c1febcc7cf4481502
yannick <yannnick.dominguez@gmail.com>
2015-03-23 13:31:36 -0700

    change nightly script to clone from nightly server

--- nightly.sh
+++ nightly.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# clone the repo needing nightly builds
-git clone git@github.com:tivo/intellij-haxe.git
-cd intellij-haxe
+# clone the nightly builds repo
+git clone git@github.com:yanhick/intellij-haxe-nightly-builds.git
+cd intellij-haxe-nightly-builds
 
-# add a remote to the repo where we will release the nightly builds
-git remote add nightly git@github.com:yanhick/intellij-haxe-nightly-builds.git
+# add a remote to the code repo
+git remote add upstream git@github.com:tivo/intellij-haxe.git
 
-# fetch the code from the nightly and merge any additional change
+# fetch the code from the code repo and merge any additional change
 # The only change will probably be to the .travis.yml file to modify the release
 # location (we want to use the nightly build repo for nightly releases)
-git fetch nightly
-git merge nightly/master
+git fetch upstream
+git merge upstream/master
 
 # create a git tag using the current time and push it to the nightly build server.
-# This would trigger a build and release from Travis.ci
+# This will trigger a build and release from Travis.ci
 DATE=`date +"%m-%d-%Y--%H-%M"`
 git tag $DATE
-git push nightly master
-git push nightly --tags
+git push origin master
+git push origin --tags
 
 # cleanup for next nightly build
 cd ../
-rm -rf intellij-haxe
+rm -rf intellij-haxe-nightly-builds
